e2f5b44078b5438bfc8193f9b2f5fb50543e81a5
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-03-15 13:34:38 +1100

    Drop configuration for items moved into base image configuration.

--- .jupyter/jupyter_kernel_gateway_config.py
+++ .jupyter/jupyter_kernel_gateway_config.py
@@ -1,4 +1,2 @@
 c.KernelGatewayApp.api = 'kernel_gateway.notebook_http'
-c.KernelGatewayApp.ip = '0.0.0.0'
-c.KernelGatewayApp.port = 8080
 c.KernelGatewayApp.seed_uri = '/opt/app-root/src/Airports.ipynb'
